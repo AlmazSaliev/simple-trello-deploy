8fb1019d593b9fe932201adfb5740409782b14dc
unknown <salievalmaz23@gmail.com>
2022-09-28 14:52:10 +0300

    deploy

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import "./App.css";
+import { WorkPages } from "./comp/WorkPages";
+import { SingUp } from "./comp/SingUp";
+import { AllPages } from "./comp/AllPages";
+import { BrowserRouter, Redirect, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <div className="container">
+      <BrowserRouter>
+        <Route path="/">
+          <Redirect to="/main" />
+        </Route>
+        <Route path="/registration" exact>
+          <SingUp />
+        </Route>
+        <Route path="/main" exact>
+          <AllPages />
+        </Route>
+        <Route path="/workpage" exact>
+          <WorkPages />
+        </Route>
+      </BrowserRouter>
+    </div>
+  );
+}
+export default App;
